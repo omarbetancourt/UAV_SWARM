9785e651363a276d88012bf03e9de530d5d1a3e6
genesisBetancourt <genesis.cruz@allata.com>
2020-06-18 13:57:25 -0700

    Added Vec2 Nmt that sums all WgtDir of swarms to targets.

--- UAV_SWARM/Main.cpp
+++ UAV_SWARM/Main.cpp
@@ -21,35 +21,43 @@ int main()
 	std::vector<Obstacle> obstacles;
 	FillTargetVector(obstacles);
 
-
+	//std::vector<Vec2> NmtVec;
 
 	std::ofstream prntpos;
 	prntpos.open ("swarmPositions.txt");
 
-
-	for (int sim_time = 0; sim_time < 3; ++sim_time) //time step. each iter is 0.001 sec
+	for (int sim_time = 0; sim_time < 1; ++sim_time) //time step. each iter is 0.001 sec
 	{
-
 		for (unsigned int mem = 0; mem < swarms.size(); mem++)
 		{
+			Vec2 Nmt(0, 0);
 			for (unsigned int tar = 0; tar < targets.size(); tar++)
 			{
 				swarms[mem].DistTar(targets[tar]);
 				swarms[mem].DirTar(targets[tar]);
 				swarms[mem].WgtDirTar();
 
+				// Printing out results for debugging purposes:
 				prntpos << "Time Step : " << sim_time << std::endl;
 				prntpos << "Target " << tar << ": " << targets[tar].GetPos().x << " , " << targets[tar].GetPos().y << std::endl;
 				prntpos << "Member " << mem << ": " <<  swarms[mem].GetPos().x << " , " << swarms[mem].GetPos().y << std::endl;
 				prntpos << "Distance : " << swarms[mem].GetDmt() << std::endl;
 				prntpos << "Direction : " << swarms[mem].GetDirTar().x << " , " << swarms[mem].GetDirTar().y << std::endl;
 				prntpos << "Wgt Dir : " << swarms[mem].GetWgtDirTar().x << " , " << swarms[mem].GetWgtDirTar().y << std::endl;
-				prntpos <<  "  " << std::endl;
-				swarms[mem].Step();
+				prntpos << "Sum Nmt_i : " << Nmt.x << " , " << Nmt.y << std::endl;
+				
+				Nmt += swarms[mem].GetWgtDirTar(); // result for Nmt for one member
+				prntpos << "Sum Nmt_i : " << Nmt.x << " , " << Nmt.y << std::endl;
+				prntpos << "  " << std::endl;
 			}
+			// add other for loops here for obstacles
+
 
 			// add Nmt into vector here??
+			swarms[mem].Step();
 		}
+
+		// if sime_time % 100 == true then print pos
 
 	}
 
