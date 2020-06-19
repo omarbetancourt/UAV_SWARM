ab00cfb9b90388b26eab75c32be02b38be763286
genesisBetancourt <genesis.cruz@allata.com>
2020-06-18 22:10:41 -0700

    Creating a .csv file for every n timesteps for ParaView use.

--- UAV_SWARM/Main.cpp
+++ UAV_SWARM/Main.cpp
@@ -4,10 +4,12 @@
 #include "Target.h"
 #include "Obstacle.h"
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
-static constexpr unsigned int nSwarmMembers = 100u; //variable available at compile time
-static constexpr unsigned int nTargets = 100u;
-static constexpr unsigned int nObstacles = 100u;
+static constexpr unsigned int nSwarmMembers = 1u; //variable available at compile time
+static constexpr unsigned int nTargets = 1u;
+static constexpr unsigned int nObstacles = 1u;
 
 #include "StartPositions.h"
 
@@ -18,15 +20,12 @@ int main()
 	std::vector<Target> targets;
 	FillTargetVector(targets);
 	std::vector<Obstacle> obstacles;
-	FillTargetVector(obstacles);
+	FillTargetVector(obstacles);  
+	
 
-	//std::vector<Vec2> NmtVec;
-
-	std::ofstream prntpos;
-	prntpos.open ("swarmPositions.txt");
-
-	for (int sim_time = 0; sim_time < 30000; ++sim_time) //time step. each iter is 0.001 sec
+	for (int sim_time = 0; sim_time < 10; ++sim_time) //time step. each iter is 0.001 sec
 	{
+
 		for (unsigned int mem = 0; mem < swarms.size(); mem++)
 		{
 			Vec2 Nmt(0, 0);
@@ -38,19 +37,9 @@ int main()
 				swarms[mem].DistTar(targets[tar]);
 				swarms[mem].DirTar(targets[tar]);
 				swarms[mem].WgtDirTar();
-
-				// Printing out results for debugging purposes:
-				prntpos << "Time Step : " << sim_time << std::endl;
-				prntpos << "Target " << tar << ": " << targets[tar].GetPos().x << " , " << targets[tar].GetPos().y << std::endl;
-				prntpos << "Member " << mem << ": " <<  swarms[mem].GetPos().x << " , " << swarms[mem].GetPos().y << std::endl;
-				prntpos << "Wgt Dir : " << swarms[mem].GetWgtDirTar().x << " , " << swarms[mem].GetWgtDirTar().y << std::endl;
-				prntpos << "Sum Nmt_i : " << Nmt.x << " , " << Nmt.y << std::endl;
 				
 				Nmt += swarms[mem].GetWgtDirTar(); // result for Nmt for one member
-				prntpos << "Sum Nmt_i : " << Nmt.x << " , " << Nmt.y << std::endl;
-				prntpos << "  " << std::endl;
 			}
-
 
 			// add other for loops here for obstacles
 
@@ -64,10 +53,15 @@ int main()
 			swarms[mem].Step(n_norm);
 		}
 
-		// if sime_time % 100 == true then print pos
-
+		if (sim_time % 2 == 0)// print every n steps = 2
+		{
+			std::stringstream ss;
+			ss << "sim-" << sim_time << ".csv";
+			std::ofstream prntpos;
+			prntpos.open(ss.str().c_str());
+			prntpos  << sim_time << std::endl;
+			prntpos.close();
+		}
 	}
-
-	prntpos.close();
 }
 
