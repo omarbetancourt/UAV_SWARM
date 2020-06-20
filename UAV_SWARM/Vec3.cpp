1791ac39d3c95ab3f3121cce224cc13d3625295e
omarbetancourt <omarbetancourt@u.boisestate.edu>
2020-06-19 21:01:43 -0700

    Created Vec3 class

---
+++ UAV_SWARM/Vec3.cpp
@@ -0,0 +1,61 @@
+#include "Vec3.h"
+
+Vec3::Vec3(float in_x, float in_y, float in_z)
+	://class initializer list
+	x(in_x),
+	y(in_y),
+	z(in_z)
+{
+}
+
+Vec3 Vec3::operator+(const Vec3& rhs) const
+{
+	return Vec3(x + rhs.x, y + rhs.y, z + rhs.z);
+}
+
+Vec3& Vec3::operator+=(const Vec3& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+	z += rhs.z;
+	return *this;
+}
+
+Vec3 Vec3::operator*(float rhs) const
+{
+	return Vec3(x * rhs, y * rhs, z * rhs);
+}
+
+Vec3& Vec3::operator*=(float rhs)
+{
+	x *= rhs;
+	y *= rhs;
+	z *= rhs;
+	return *this;
+}
+
+Vec3 Vec3::operator/(float rhs) const
+{
+	return Vec3(x / rhs, y / rhs, z / rhs);
+}
+
+Vec3& Vec3::operator/=(float rhs)
+{
+	x /= rhs;
+	y /= rhs;
+	z /= rhs;
+	return *this;
+}
+
+Vec3 Vec3::operator-(const Vec3& rhs) const
+{
+	return Vec3(x - rhs.x, y - rhs.y, z - rhs.z);
+}
+
+Vec3& Vec3::operator-=(const Vec3& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+	z -= rhs.z;
+	return *this;
+}
---
+++ UAV_SWARM/Vec3.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Vec3
+{
+public:
+	Vec3() = default; // do nothing constructor
+	Vec3(float in_x, float in_y, float z);
+	Vec3 operator+(const Vec3& rhs) const; // lhs + rhs
+	Vec3& operator+=(const Vec3& rhs);
+
+	Vec3 operator*(float rhs) const;
+	Vec3& operator*=(float rhs);
+
+	Vec3 operator/(float rhs) const;
+	Vec3& operator/=(float rhs);
+
+	Vec3 operator-(const Vec3& rhs) const;
+	Vec3& operator-=(const Vec3& rhs);
+
+public:
+	float x;
+	float y;
+	float z;
+};
